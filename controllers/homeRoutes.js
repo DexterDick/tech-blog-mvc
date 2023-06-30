75256a7cd960b5f8ae0052cea2a0071f2da55a5d
Dexter Dick <@dexterdick@hotmail.com>
2023-06-29 21:21:48 -0400

    started coding home controller homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,55 @@
 const router = require("express").Router();
 
-const { User } = require("../models");
+const { User, Blog, Comments } = require("../models");
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ["username"],
+                },
+            ],
+        });
+
+        if (!blogData) {
+            res.status(404).json({ message: "No blog data found!" });
+            return;
+        }
+
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+        res.status(200).json(blogs);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        const blogData = await Blog.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ["username"],
+                },
+            ],
+        });
+
+        if (!blogData) {
+            res.status(404).json({ message: "No blog data found with ID!" });
+            return;
+        }
+
+        const blog = blogData.get({ plain: true });
+
+        res.status(200).json(blog);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
