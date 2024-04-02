94c9751cc92fab1c72433de733a72ee4fdfdfe87
Dexter Dick <@dexterdick@hotmail.com>
2024-04-02 04:38:38 -0400

    made updates to post routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,8 +11,30 @@ router.get("/", async (req, res) => {
         });
 
         const posts = postData.map((post) => post.get({ plain: true }));
-        console.log(posts);
+
         res.render("all-posts", { posts, logged_in: req.session.logged_in });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get("/post/:id", async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                { model: User, attributes: { exclude: ["password"] } },
+                { model: Comment },
+            ],
+        });
+        if (postData) {
+            const post = postData.get({ plain: true });
+            console.log(post);
+            res.render("single-post", {
+                post,
+            });
+        } else {
+            res.status(404).end();
+        }
     } catch (err) {
         res.status(500).json(err);
     }
